d5468ce6bb68b1da836e4243b78b7ca36ebec7c5
DESKTOP-FSQ5AJJ\yujin <2lee6527@naver.com>
2021-09-20 23:40:18 +0900

    ver6을 위한 커밋

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 #opencv library
-OPENCVROOT:= D:\AndroidStudioProjects\EMarket\sdk
+OPENCVROOT:= C:\GraduationDesign_android_git\sdk
 OPENCV_CAMERA_MODULES:=on
 OPENCV_INSTALL_MODULES:=on
 OPENCV_LIB_TYPE:=SHARED
